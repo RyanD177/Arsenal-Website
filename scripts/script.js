eef06aa985f49dc80be26079959c287189a0b47c
Ryan Dixon <113196071+RyanD177@users.noreply.github.com>
2023-10-29 23:59:50 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
